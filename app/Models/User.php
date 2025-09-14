c6594bfa1c723892691adf1038dfe0a31a6d2eff
gerrygeop <gerryja28@gmail.com>
2025-09-14 22:01:29 +0800

    feat: add soft deletes

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,13 +6,14 @@ namespace App\Models;
 
 use Filament\Models\Contracts\HasName;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable implements HasName
 {
-    use HasFactory, Notifiable, HasRoles;
+    use HasFactory, Notifiable, HasRoles, SoftDeletes;
 
     protected $fillable = [
         'name',
