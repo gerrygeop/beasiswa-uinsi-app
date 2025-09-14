f9c1296b0534c44c64a0634d20900a1c9cb9db95
gerrygeop <gerryja28@gmail.com>
2025-09-14 22:02:02 +0800

    feat: add user policy and enhance beasiswa policy

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -2,26 +2,25 @@
 
 namespace App\Policies;
 
-use App\Models\Beasiswa;
 use App\Models\User;
 use Illuminate\Auth\Access\Response;
 
-class BeasiswaPolicy
+class UserPolicy
 {
     /**
      * Determine whether the user can view any models.
      */
     public function viewAny(User $user): bool
     {
-        return $user->hasAnyRole(['mahasiswa', 'staf']);
+        return false;
     }
 
     /**
      * Determine whether the user can view the model.
      */
-    public function view(User $user, Beasiswa $beasiswa): bool
+    public function view(User $user, User $model): bool
     {
-        return $user->hasAnyRole(['mahasiswa', 'staf']);
+        return false;
     }
 
     /**
@@ -29,29 +28,29 @@ class BeasiswaPolicy
      */
     public function create(User $user): bool
     {
-        return $user->hasAnyRole(['mahasiswa', 'staf']);
+        return false;
     }
 
     /**
      * Determine whether the user can update the model.
      */
-    public function update(User $user, Beasiswa $beasiswa): bool
+    public function update(User $user, User $model): bool
     {
-        return $user->hasAnyRole(['mahasiswa', 'staf']);
+        return false;
     }
 
     /**
      * Determine whether the user can delete the model.
      */
-    public function delete(User $user, Beasiswa $beasiswa): bool
+    public function delete(User $user, User $model): bool
     {
-        return $user->hasAnyRole(['mahasiswa', 'staf']);
+        return false;
     }
 
     /**
      * Determine whether the user can restore the model.
      */
-    public function restore(User $user, Beasiswa $beasiswa): bool
+    public function restore(User $user, User $model): bool
     {
         return false;
     }
@@ -59,7 +58,7 @@ class BeasiswaPolicy
     /**
      * Determine whether the user can permanently delete the model.
      */
-    public function forceDelete(User $user, Beasiswa $beasiswa): bool
+    public function forceDelete(User $user, User $model): bool
     {
         return false;
     }
