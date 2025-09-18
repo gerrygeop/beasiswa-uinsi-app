6cbfc3d21a8ff431b5723126c3dc68c452596f15
gerrygeop <gerryja28@gmail.com>
2025-09-19 01:43:21 +0800

    feat: update brand name

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -6,7 +6,6 @@ use App\Filament\Pages\Auth\EditProfile;
 use App\Filament\Pages\Auth\LoginCustom;
 use App\Filament\Pages\Dashboard;
 use App\Filament\Widgets\MahasiswaStatsOverview;
-use App\Models\Mahasiswa;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\AuthenticateSession;
 use Filament\Http\Middleware\DisableBladeIconComponents;
@@ -16,7 +15,6 @@ use Filament\Panel;
 use Filament\PanelProvider;
 use Filament\Support\Colors\Color;
 use Filament\Widgets\AccountWidget;
-use Filament\Widgets\FilamentInfoWidget;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 use Illuminate\Cookie\Middleware\EncryptCookies;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
@@ -37,6 +35,7 @@ class AdminPanelProvider extends PanelProvider
             ->colors([
                 'primary' => Color::Emerald,
             ])
+            ->brandName('SIBEA')
             ->databaseNotifications()
             ->databaseNotificationsPolling(null)
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\Filament\Resources')
